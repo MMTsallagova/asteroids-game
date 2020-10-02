822304c1b42e5f7b142a7343a6d58f6cf7024497
MMTsallagova <55704942+MMTsallagova@users.noreply.github.com>
2020-10-02 22:44:23 +0300

    Task_1
    - Add Background
    - Add move and rotate to player

---
+++ Asteroid_1979/Assets/Player/PlayerScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public Vector2 speed = new Vector2(5,1);
+    private Vector3 movement;
+    private Vector2 rotation;
+
+    public Rigidbody2D rb;
+    float inputX;
+    float inputY;
+    void Update()
+    {
+        inputX = Input.GetAxis("Horizontal");
+        inputY = Input.GetAxis("Vertical");
+        movement = new Vector3(0, inputY * speed.y, 0);
+
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            Vector3 newPos = transform.position + transform.up * speed.y;
+            transform.position = newPos;
+        }
+
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            Vector3 newPos = transform.position - transform.up * speed.y;
+            transform.position = newPos;
+        }
+
+        float angle = inputX * speed.x;
+        transform.Rotate(0, 0, -angle);
+
+
+    }
+
+}
