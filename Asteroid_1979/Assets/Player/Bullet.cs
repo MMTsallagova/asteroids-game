a009c698d0801d088f5ede6ee9e3579ffd9c832d
MMTsallagova <55704942+MMTsallagova@users.noreply.github.com>
2020-10-03 15:17:04 +0300

    Add fire
    - Add Bullet
    - Add fire point
    - add sound

---
+++ Asteroid_1979/Assets/Player/Bullet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 4;
+
+    void Start()
+    {
+        transform.position = GameObject.Find("Muzzle").transform.position;
+        transform.up = GameObject.Find("Muzzle").transform.up;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPos = transform.position + transform.up * speed * Time.deltaTime;
+        transform.position = newPos;
+    }
+}
+
